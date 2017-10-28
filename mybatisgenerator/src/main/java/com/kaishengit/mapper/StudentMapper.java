450b99e949adc01b703f82f2bdd6237f64d60d45
caoxianggaol <xiaogao815224@163.com>
2017-10-28 10:57:28 +0800

    逆向工程中targetRuntime=MyBatis3Simple时自动生成代码示例

---
+++ mybatisgenerator/src/main/java/com/kaishengit/mapper/StudentMapper.java
@@ -0,0 +1,16 @@
+package com.kaishengit.mapper;
+
+import com.kaishengit.entity.Student;
+import java.util.List;
+
+public interface StudentMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Student record);
+
+    Student selectByPrimaryKey(Integer id);
+
+    List<Student> selectAll();
+
+    int updateByPrimaryKey(Student record);
+}
